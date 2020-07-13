1f4397fadc435014eeba6a57a612c4be032a5939
alailsonko <aalailson3@gmail.com>
2020-07-13 18:25:40 -0300

    feat: ensure that password, email, handle are validated

--- backend/firebase-functions/functions/index.js
+++ backend/firebase-functions/functions/index.js
@@ -56,6 +56,20 @@ app.get('/screams', (req, res) => {
   })
  })
 
+ const isEmail = (email) => {
+   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+   
+   if(email.match(regEx)){ 
+     return true
+  } else {
+    return false;
+  }
+}
+
+ const isEmpty = (string) => {
+   if(string.trim() === '' ) return true;
+   else return false;
+ }
 
 // Signup route
 app.post('/signup', (req, res) => {
@@ -65,6 +79,22 @@ app.post('/signup', (req, res) => {
     confirmPassword: req.body.confirmPassword,
     handle: req.body.handle
   }
+
+  let errors = {}
+
+  if(isEmpty(newUser.email)){
+    errors.email = 'Email must not be empty'
+  } else if(!isEmail(newUser.email)){
+           errors.email = 'Must be a valid email adress'
+
+  }
+
+  if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match'
+  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
+
+  if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
   // TODO validate data
   let token, userId;
   db.doc(`/users/${newUser.handle}`)
