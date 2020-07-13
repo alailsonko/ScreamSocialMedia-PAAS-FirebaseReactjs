81dd7a473e5f6193ce23ae6ebea9300a9ca7bdf1
alailsonko <aalailson3@gmail.com>
2020-07-13 13:10:04 -0300

    chore: initializng firebase aithentication

--- backend/firebase-functions/functions/index.js
+++ backend/firebase-functions/functions/index.js
@@ -1,12 +1,23 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
-
+const firebase = require('firebase')
+const app = require('express')()
  
+
 admin.initializeApp()
 
-
-const express = require('express')
-const app = express()
+var firebaseConfig = {
+  apiKey: "AIzaSyAcTWzC7__R3t2rtoifcqN4fDBz-z5VFDI",
+  authDomain: "mansion-4fa4a.firebaseapp.com",
+  databaseURL: "https://mansion-4fa4a.firebaseio.com",
+  projectId: "mansion-4fa4a",
+  storageBucket: "mansion-4fa4a.appspot.com",
+  messagingSenderId: "396090854471",
+  appId: "1:396090854471:web:d2a51eff6313f0e5fd3562",
+  measurementId: "G-XHBMDZGRT7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 app.get('/screams', (req, res) => {
   admin
@@ -53,8 +64,9 @@ app.get('/screams', (req, res) => {
   })
  })
 
- 
 
-// https://baseurl.com/api/<something>
+// Signup route
+app.post('/signup')
+
 
 exports.api = functions.region('us-central1').https.onRequest(app)
