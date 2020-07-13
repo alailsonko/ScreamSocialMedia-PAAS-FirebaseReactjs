568058238b374f7c9563bb60d28ec47790968076
alailsonko <aalailson3@gmail.com>
2020-07-13 12:46:25 -0300

    chore: creating one dbschema

---
+++ backend/firebase-functions/dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: 'user',
+      body: 'this is the scream today',
+      createdAt: '2020-07-13T11:46:03:0129Z',
+      likeCount: 5,
+      commentCount: 2
+    }
+  ]
+}
