68fa9ca2471b3033af17d47cf6ef5b2adb9e7d03
alailsonko <aalailson3@gmail.com>
2020-07-13 12:47:35 -0300

    feat: sorting screams

--- backend/firebase-functions/functions/index.js
+++ backend/firebase-functions/functions/index.js
@@ -9,11 +9,20 @@ const express = require('express')
 const app = express()
 
 app.get('/screams', (req, res) => {
-  admin.firestore().collection('screams').get()
+  admin
+  .firestore()
+  .collection('screams')
+  .orderBy('createdAt', 'desc')
+  .get()
   .then(data => {
     let screams = []
     data.forEach(doc => {
-      screams.push(doc.data())
+      screams.push({
+        screamId: doc.id,
+        body: doc.data().body,
+        userHandle: doc.data().userHandle,
+        createdAt: doc.data().createdAt,
+      })
     })
     return res.json(screams)
   })
@@ -24,19 +33,20 @@ app.get('/screams', (req, res) => {
 
  app.post('/scream', (req, res) => {
   const newScream = {
-    body: req.body.body,
-    userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    
+    "body": req.body.body,
+    "userHandle": req.body.userHandle,
+    "createdAt": new Date().toISOString()
   }
 
   admin.firestore()
   .collection('screams')
   .add(newScream)
-  then((doc) => {
-    res.json({message: `document ${doc.id} created successfully`})
+  .then((doc) => {
+   return res.json({"message": `document ${doc.id} created successfully`})
   })
   .catch(err => {
-    res.status(500).json({ error: 'something went wrong' })
+    res.status(500).json({ "error": "something went wrong" })
     console.error(err)
   })
  })
@@ -45,4 +55,4 @@ app.get('/screams', (req, res) => {
 
 // https://baseurl.com/api/<something>
 
-exports.api = functions.https.onRequest(app)
+exports.api = functions.region('us-central1').https.onRequest(app)
