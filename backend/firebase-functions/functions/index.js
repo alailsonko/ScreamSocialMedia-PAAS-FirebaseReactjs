92c1d4a6fff5283ac879470da73779e78c968732
alailsonko <aalailson3@gmail.com>
2020-07-14 08:36:34 -0300

    refactor: cleaning code and chaging code of the users routes to one different directory

--- backend/firebase-functions/functions/index.js
+++ backend/firebase-functions/functions/index.js
@@ -3,7 +3,7 @@ const firebase = require('firebase')
 const app = require('express')()
 var firebaseConfig = require('./firebaseConfig')
 const { getAllScreams, postOneScream } = require('./handlers/screams')
- 
+const { signup, login } = require('./handlers/users') 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig.firebaseConfig);
 
@@ -11,6 +11,11 @@ firebase.initializeApp(firebaseConfig.firebaseConfig);
 // scream routes
 app.get('/screams', getAllScreams)
 app.post('/scream', FBAuth, postOneScream)
+
+//users routes
+app.post('/signup', signup )
+app.post('/login', login)
+
 
 const FBAuth = (req, res, next) => {
   let idToken;
@@ -54,101 +59,6 @@ const FBAuth = (req, res, next) => {
    else return false;
  }
 
-// Signup route
-app.post('/signup', (req, res) => {
-  const newUser = {
-    email: req.body.email,
-    password: req.body.password,
-    confirmPassword: req.body.confirmPassword,
-    handle: req.body.handle
-  }
-
-  let errors = {}
-
-  if(isEmpty(newUser.email)){
-    errors.email = 'Email must not be empty'
-  } else if(!isEmail(newUser.email)){
-           errors.email = 'Must be a valid email adress'
-
-  }
-
-  if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
-  if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match'
-  if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty'
-
-  if(Object.keys(errors).length > 0) return res.status(400).json(errors);
-
-  // TODO validate data
-  let token, userId;
-  db.doc(`/users/${newUser.handle}`)
-  .get()
-  .then(doc => {
-    if(doc.exists){
-      return res.status(400).json({ handle: 'this handle is already taken' })
-    } else {
-     return firebase
-      .auth()
-      .createUserWithEmailAndPassword(newUser.email, newUser.password)
-    }
-  })
-  .then(data => {
-    userId = data.user.uid
-    return data.user.getIdToken()
-  })
-  .then(idToken => {
-    token = idToken;
-    const userCredentials = {
-      handle: newUser.handle,
-      email: newUser.email,
-      createdAt: new Date().toISOString(),
-      userId
-    }
-    return db.doc(`/users/${newUser.handle}`).set(userCredentials)
-  
-  })
-  .then(() => {
-    return res.status(201).json({ token })
-  })
-  .catch(err => {
-    console.error(err)
-    if(err.code === 'auth/email-already-in-use'){
-      return res.status(400).json({ email: 'Email is already in use' })
-    } else {
-      return res.status(500).json({ error: err.code })
-    }
-  })
-
  
-})
-
-// login user
-  app.post('/login', (req, res) => {
-  const user = {
-    email: req.body.email,
-    password: req.body.password
-  }
-  let errors = {}
-
-  if(isEmpty(user.email)) errors.email = 'Must not be empty'
-  if(isEmpty(user.password)) errors.password = 'Must not be empty'
-
-  if(Object.keys(errors).length > 0) return res.status(400).json(errors)
-
-  firebase.auth().signInWithEmailAndPassword(user.email, user.password)
-  .then(data => {
-    return data.user.getIdToken()
-  })
-  .then(token => {
-    return res.json({ token })
-  })
-  .catch(err => {
-    console.error(err)
-    if( err.code === 'auth/wrong-password') {
-      return res.status(403).json({ general: 'wrong credentials, please try again' })
-    } else {
-    return res.status(500).json({ error: err.code })
-    }
-  })
-})
 
 exports.api = functions.region('us-central1').https.onRequest(app)
